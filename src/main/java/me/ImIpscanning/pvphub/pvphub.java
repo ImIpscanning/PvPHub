1c3e3f2e6775de814ed000bd6edfb7a9538cd6e0
ImIpscanning <mavchin8@gmail.com>
2026-04-24 02:30:16 +0200

    Create pvphub.java

---
+++ src/main/java/me/ImIpscanning/pvphub/pvphub.java
@@ -0,0 +1,59 @@
+package me.ImIpscanning.pvphub;
+
+import org.bukkit.Bukkit;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PvPHub extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("PvPHub activado");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        if (!(sender instanceof Player)) return true;
+        Player p = (Player) sender;
+
+        if (cmd.getName().equalsIgnoreCase("pvphub")) {
+
+            if (args.length == 0) {
+                p.sendMessage("§aModos: trigger | semispam | spam");
+                return true;
+            }
+
+            switch (args[0].toLowerCase()) {
+
+                case "trigger":
+                    setAttackSpeed(p, 4.0);
+                    p.sendMessage("§aModo Trigger (~4 CPS)");
+                    break;
+
+                case "semispam":
+                    setAttackSpeed(p, 8.0);
+                    p.sendMessage("§eModo SemiSpam (~8 CPS)");
+                    break;
+
+                case "spam":
+                    setAttackSpeed(p, 20.0);
+                    p.sendMessage("§cModo Spam (sin cooldown)");
+                    break;
+
+                default:
+                    p.sendMessage("§cModo inválido");
+                    break;
+            }
+        }
+
+        return true;
+    }
+
+    private void setAttackSpeed(Player p, double speed) {
+        p.getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(speed);
+    }
+}
